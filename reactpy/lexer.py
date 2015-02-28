350c7ce9888969c653c42b07fee4884f7265eb26
Daniele Esposti <daniele.esposti@plentific.com>
2015-02-28 01:58:40 +0000

    Extended lexer

--- reactpy/lexer.py
+++ reactpy/lexer.py
@@ -3,33 +3,53 @@ from __future__ import unicode_literals, print_function
 import ply.lex as lex
 
 
-reserved = {
-    'var': 'VAR',
-}
-
-
-tokens = reserved.values() + [
-    'ID',
-    'INTEGER',
-    'EQUALS',
-]
-
-
 t_ignore = r' \t'
 
 
-t_EQUALS = r'='
+keywords = {
+    'var': 'VAR',
+    'function': 'FUNCTION',
+    'return': 'RETURN',
+}
 
 
-def t_INTEGER(t):
+tokens = keywords.values() + [
+    # Identifiers
+    'ID',
+
+    # Assignment
+    'EQUALS',
+
+    # Delimiters
+    'SEMI', 'COLO', 'COMMA', 'DOT', 'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE',
+
+    # Numeric const
+    'INTEGER_CONST'
+]
+
+# Assignment
+t_EQUALS    = r'='
+
+# Delimiters
+t_SEMI      = r';'
+t_COLO      = r':'
+t_COMMA      = r','
+t_DOT       = r'\.'
+t_LPAREN    = r'\('
+t_RPAREN    = r'\)'
+t_LBRACE    = r'\{'
+t_RBRACE    = r'\}'
+
+# Keywords
+def t_INTEGER_CONST(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-
+# Identifiers
 def t_ID(t):
     r'[a-zA-Z]+'
-    t.type = reserved.get(t.value, 'ID')
+    t.type = keywords.get(t.value, 'ID')
     return t
 
 
--- tests/lexer_tests.py
+++ tests/lexer_tests.py
@@ -10,35 +10,41 @@ class LexerTests(unittest.TestCase):
     def setUp(self):
         self.lexer = lexer
 
-    def assertTokensEqual(self, data, *expected):
-        print('========')
-        print(data)
-        print('--------')
+    def assertTokensEqual(self, data, *args):
         self.lexer.input(data)
 
-        for type_, value in expected:
+        for expected in args:
             token = next(self.lexer)
-            print(token)
 
-            self.assertEqual(token.type, type_)
-            self.assertEqual(token.value, value)
+            if isinstance(expected, (list, tuple)):
+                type_, value = expected
+
+                self.assertEqual(token.type, type_)
+                self.assertEqual(token.value, value)
+            else:
+                self.assertEqual(token.type, expected)
 
         with self.assertRaises(StopIteration):
-            print('Token left in the lexer:', next(self.lexer))
+            next(self.lexer)
 
-        print('========')
+    def test_integer_const_tokens(self):
+        self.assertTokensEqual('1', ('INTEGER_CONST', 1))
 
     def test_id_tokens(self):
-        """
-        var <identifier>;
-        """
-        self.assertTokensEqual(
-            'var myVariable',
-            ('VAR', 'var'), ('ID', 'myVariable')
-        )
-        self.assertTokensEqual(
-            'var myVariable = 1',
-            ('VAR', 'var'), ('ID', 'myVariable'),
-            ('EQUALS', '='),
-            ('INTEGER', 1)
-        )
+        self.assertTokensEqual('var myVariable', 'VAR', ('ID', 'myVariable'))
+        self.assertTokensEqual('myVariable', ('ID', 'myVariable'))
+
+    def test_keywords_tokens(self):
+        self.assertTokensEqual('var myVariable', 'VAR', ('ID', 'myVariable'))
+        self.assertTokensEqual('function', 'FUNCTION')
+        self.assertTokensEqual('return', 'RETURN')
+
+    def test_separators_tokens(self):
+        self.assertTokensEqual(';', 'SEMI')
+        self.assertTokensEqual(':', 'COLO')
+        self.assertTokensEqual(',', 'COMMA')
+        self.assertTokensEqual('.', 'DOT')
+        self.assertTokensEqual('{', 'LBRACE')
+        self.assertTokensEqual('}', 'RBRACE')
+        self.assertTokensEqual('(', 'LPAREN')
+        self.assertTokensEqual(')', 'RPAREN')
