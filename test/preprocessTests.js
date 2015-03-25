67e6d0932b2026a5185a5335e3ca9ca25d7caf2f
Daniele Esposti <daniele.esposti@plentific.com>
2015-03-25 19:41:55 +0000

    Refactored annotator and visitor

--- test/preprocessTests.js
+++ test/preprocessTests.js
@@ -1,37 +1,61 @@
-var assert = require("assert");
-var esprima = require('esprima-fb');
-var should = require('should');
+var _       = require('lodash');
+    assert  = require("assert");
+    esprima = require('esprima-fb');
+    should  = require('should');
 
-var preprocessor = require("../lib/preprocessor");
+    preprocessor = require("../lib/preprocessor");
 
 
 describe('Preprocess', function () {
+    function parse(code) {
+        return preprocessor.annotate(esprima.parse(code));
+    }
+
     describe('#filterReactClass', function () {
         it('includes React.class()', function () {
             // Pre-process AST
-            var expected = esprima.parse(
-                'var myClass = React.createClass({});'
-            );
+            var expected = parse('var myClass = React.createClass({});');
             var ast = preprocessor.filterReactCreateClass(expected);
 
             // Check
             ast.should.eql(expected);
         });
 
-        it('drops anything else except react.createClass()', function () {
+        it('drops anything else except React.createClass()', function () {
             // Pre-process AST
             var ast = preprocessor.filterReactCreateClass(
-                esprima.parse(
+                parse(
+                    'React.render();' +
                     'var a = 42;' +
                     'var myClass = React.createClass({});'
                 )
             );
-            var expected = esprima.parse(
-                'var myClass = React.createClass({});'
-            );
+            var expected = parse('var myClass = React.createClass({});');
 
             // Check
             ast.should.eql(expected);
+        });
+    });
+
+    describe('#annotate', function () {
+        it('annotates all the nodes with children() method', function () {
+            var ast,
+                node,
+                nodes = [];
+
+            ast = parse('var myClass = React.createClass({});');
+            nodes = [ast];
+
+            while (nodes.length) {
+                node = nodes.pop();
+
+                _.isFunction(node.children).should.be.true;
+                node.children.should.be.Function;
+
+                nodes.concat(node.children());
+            }
+
+            return ast;
         });
     });
 });
